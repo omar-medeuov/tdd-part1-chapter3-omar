dfacb33d7324a9171cb3567e848923fd6fca759c
omar-medeuov <omarmedeuov@gmail.com>
2025-12-20 19:35:50 +0500

    complete App Structure chapter

--- project/app/main.py
+++ project/app/main.py
@@ -1,21 +1,17 @@
 import os
+import logging
 
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
+from app.db import init_db
 
+log=logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
 
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True
-    )
 
     application.include_router(ping.router)
 
@@ -23,3 +19,5 @@ def create_application() -> FastAPI:
 
 
 app = create_application()
+
+init_db(app)
