5b9bb92b3ec45bcaaa67ba9f23798834c3b447d3
omar-medeuov <omarmedeuov@gmail.com>
2025-12-20 21:58:36 +0500

    finish RESTful Routes chapter

--- project/app/main.py
+++ project/app/main.py
@@ -4,7 +4,7 @@ import logging
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log=logging.getLogger("uvicorn")
@@ -14,6 +14,7 @@ def create_application() -> FastAPI:
 
 
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
